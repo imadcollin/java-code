c7021a8459194309c33686a56a8059765b9e97d5
Collin Imad <imad.collin@prv.se>
2021-11-09 12:44:16 +0100

    Fix check method

--- src/main/java/booleans/BooleanOrder.java
+++ src/main/java/booleans/BooleanOrder.java
@@ -2,51 +2,74 @@ package booleans;
 
 import java.math.BigInteger;
 
-class Order {
+class BooleanOrder {
+    public static int counter = 0;
 
-        public static int counter = 0;
+    public BooleanOrder(final String operands, final String operators) {
+      /*  boolean[] booleansArr = new boolean[operands.length()];
+        for (int i = 0; i < booleansArr.length; i++) {
+            if ((operands.charAt(i)) == 't') booleansArr[i] = true;
+            else if ((operands.charAt(i)) == 'f') booleansArr[i] = false;
+            else System.out.println("something wrong...!");
+        }
 
-        public void BooleanOrder(final String operands, final String operators) {
-            boolean[] booleansArr = new boolean[operands.length()];
-            for (int i = 0; i < booleansArr.length; i++) {
-                if ((operands.charAt(i)) == 't') booleansArr[i] = true;
-                else if ((operands.charAt(i)) == 'f') booleansArr[i] = false;
-                else System.out.println("something wrong...!");
-            }
+        for (int i = 0; i < booleansArr.length; i++) {
+            if (i < booleansArr.length - 1) {
+                print(booleansArr[i], operators.charAt(0), booleansArr[i + 1]);
 
-            for (int i = 0; i < booleansArr.length; i++) {
-                if (i < booleansArr.length - 1) {
-                    print(booleansArr[i], operators.charAt(0), booleansArr[i + 1]);
+            } else
+                print(booleansArr[booleansArr.length - 1], operators.charAt(operators.length() - 1), booleansArr[booleansArr.length - 2]);
 
+        }
+       */
+    }
+
+    public void BooleanOrder(final String operands, final String operators) {
+        boolean[] operandsArr = sortedArr(operands);
+
+
+        for (int i = 0; i < operandsArr.length; i++) {
+            for (int j = 0; j < operators.length(); j++) {
+                if (i < operandsArr.length - 1) {
+                    print(operandsArr[i], operators.charAt(0), operandsArr[i + 1]);
                 } else
-                    print(booleansArr[booleansArr.length - 1], operators.charAt(operators.length() - 1), booleansArr[booleansArr.length - 2]);
+                    print(operandsArr[operandsArr.length - 1], operators.charAt(operators.length() - 1), operandsArr[operandsArr.length - 2]);
 
             }
         }
+    }
 
-        public  void print(boolean en, char ch, boolean sec) {
-
-            switch (ch) {
-                case '&':
-                    System.out.print(en + " " + ch + " " + sec + ":" + (en & sec));
-                    if (en & sec) counter++;
-                    break;
-                case '^':
-                    System.out.println(en + " " + ch + " " + sec + ":" + (en ^ sec));
-                    if (en ^ sec) counter++;
-                    break;
-                case '|':
-                    if (en | sec) counter++;
-                    System.out.println(en + " " + ch + " " + sec + ":" + (en | sec));
-
-                default:
-                    System.out.println("Nothing to print... ! ");
-            }
+    public boolean[] sortedArr(String operands) {
+        boolean[] booleansArr = new boolean[operands.length()];
+        for (int i = 0; i < booleansArr.length; i++) {
+            if ((operands.charAt(i)) == 't') booleansArr[i] = true;
+            else if ((operands.charAt(i)) == 'f') booleansArr[i] = false;
+            else System.out.println("something wrong...!");
         }
+        return booleansArr;
+    }
 
-        public BigInteger solve() {
-            // Your code here
-            return BigInteger.valueOf(counter);
+    public void print(boolean en, char ch, boolean sec) {
+
+        switch (ch) {
+            case '&':
+                System.out.println(en + " " + ch + " " + sec + ":" + (en & sec));
+                if (en & sec) counter++;
+                break;
+            case '^':
+                System.out.println(en + " " + ch + " " + sec + ":" + (en ^ sec));
+                if (en ^ sec) counter++;
+                break;
+            case '|':
+                if (en | sec) counter++;
+                System.out.println(en + " " + ch + " " + sec + ":" + (en | sec));
+
+            default:
+                System.out.println("Nothing to print... ! " + ch);
         }
+    }
+
+    public BigInteger solve() {
+        return BigInteger.valueOf(counter);
     }
 }
--- src/main/java/lists/Survivor.java
+++ src/main/java/lists/Survivor.java
@@ -1,14 +1,16 @@
 package lists;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Survivor {
     public static <T> List<T> josephusPermutation(final Object[] items, final int k) {
         List<T> newList = new ArrayList<>();
         boolean flag = false;
         int count = 0;
-        for (int i = 0; i < items.length; i++) {
+        for (int i = 0; i < k; i++) {
             count++;
             if (count == 2) {
                 count = 0;
@@ -19,4 +21,30 @@ public class Survivor {
         return newList;
     }
 
+    public static <T> Object[] check(Object[] items, int i) {
+
+        int count = i;
+
+        List<Object> myList = new ArrayList<>();
+
+        for (int j = 0; j < items.length; j++) {
+
+            if (count > items.length) {
+                count = (count % 7);
+
+            }
+            myList.add(items[count - 1]);
+
+            count = count + 3;
+        }
+        System.out.println(myList);
+        return myList.toArray();
+    }
+
+    private static <T> List<Object> generateNewList(Object[] items, Object item) {
+        System.out.println(item);
+        System.out.println(Arrays.stream(items).filter(x -> !x.equals(item)).collect(Collectors.toList()));
+
+        return Arrays.stream(items).filter(x -> !x.equals(item)).collect(Collectors.toList());
+    }
 }
