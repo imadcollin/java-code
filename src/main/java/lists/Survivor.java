5b927fe0fe8099864353b8e7fdde175276697181
Collin Imad <imad.collin@prv.se>
2021-11-10 11:29:48 +0100

    Refactoring the logic and test

--- src/main/java/lists/Survivor.java
+++ src/main/java/lists/Survivor.java
@@ -6,23 +6,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Survivor {
-    public static <T> List<T> josephusPermutation(final Object[] items, final int k) {
-        List<T> newList = new ArrayList<>();
-        boolean flag = false;
-        int count = 0;
-        for (int i = 0; i < k; i++) {
-            count++;
-            if (count == 2) {
-                count = 0;
-                newList.add((T) items[i]);
-                break;
+    public static <T> Object josephusPermutation(final Object[] items, final int i) {
+        int count = i;
+
+        List<Object> myList = new ArrayList<>();
+        for (int j = 0; j < items.length; j++) {
+
+            if (count > items.length) {
+                count = (count % 7);
+
             }
+            myList.add(items[count - 1]);
+
+            count = count + 3;
         }
-        return newList;
+
+        return myList.get(0);
     }
 
-    public static <T> Object[] check(Object[] items, int i) {
 
+    public static <T> Object[] check(final Object[] items, final int i) {
         int count = i;
 
         List<Object> myList = new ArrayList<>();
@@ -37,14 +40,6 @@ public class Survivor {
 
             count = count + 3;
         }
-        System.out.println(myList);
         return myList.toArray();
-    }
-
-    private static <T> List<Object> generateNewList(Object[] items, Object item) {
-        System.out.println(item);
-        System.out.println(Arrays.stream(items).filter(x -> !x.equals(item)).collect(Collectors.toList()));
-
-        return Arrays.stream(items).filter(x -> !x.equals(item)).collect(Collectors.toList());
     }
 }
