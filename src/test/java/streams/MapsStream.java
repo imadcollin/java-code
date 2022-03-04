89c30ef9e24f194b9449f1ef92daa539b69c5319
imadcollin <imad.collin@gmail.com>
2022-03-04 21:20:17 +0100

    Add another map example

--- src/test/java/streams/MapsStream.java
+++ src/test/java/streams/MapsStream.java
@@ -1,5 +1,6 @@
 package streams;
 
+import java.util.AbstractMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -12,5 +13,101 @@ public class MapsStream {
 
     protected Optional<String> getMatch(Map<String, String> map, String lkey) {
         return map.entrySet().stream().filter(x -> x.getKey().equals(lkey)).map(y -> y.getValue()).findFirst();
+    }
+
+    protected List<Teacher> ListObjectExchange(List<Student> students) {
+        return students.stream().map(student -> {
+            Teacher teacher = new Teacher(student.getId(), student.getName(), student.getAge(), 3500);
+            return teacher;
+        }).collect(Collectors.toList());
+    }
+
+    protected Map<String, Integer> transfer(Map<String, String> map) {
+        return map.entrySet().stream().map(x -> new AbstractMap.SimpleEntry<>(
+                x.getKey(), Integer.parseInt(x.getValue()))).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    public static class Student {
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public Student(int id, String name, int age) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        private int id;
+        private String name;
+        private int age;
+    }
+
+    public class Teacher {
+        private int id;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public double getSalary() {
+            return salary;
+        }
+
+        public void setSalary(double salary) {
+            this.salary = salary;
+        }
+
+        private String name;
+
+        public Teacher(int id, String name, int age, double salary) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        private int age;
+        private double salary;
     }
 }
--- src/test/java/streams/MapsStreamTest.java
+++ src/test/java/streams/MapsStreamTest.java
@@ -3,7 +3,9 @@ package streams;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -35,5 +37,29 @@ class MapsStreamTest {
 
         assertEquals("two", mapsStream.getMatch(map, "2").get());
         assertEquals("three", mapsStream.getMatch(map, "3").get());
+    }
+
+    @Test
+    void testExchange() {
+        MapsStream.Student s1 = new MapsStream.Student(1, "Bob", 31);
+        MapsStream.Student s2 = new MapsStream.Student(2, "Eva", 35);
+        MapsStream.Student s3 = new MapsStream.Student(3, "Joel", 26);
+        List<MapsStream.Student> students = Arrays.asList(s1, s2, s2);
+
+        assertEquals(3, mapsStream.ListObjectExchange(students).size());
+        assertEquals("Bob", mapsStream.ListObjectExchange(students).get(0).getName());
+        assertEquals("Eva", mapsStream.ListObjectExchange(students).get(1).getName());
+    }
+
+    @Test
+    void testTransform() {
+        Map<String, String> map = new HashMap<>();
+        map.put("One", "1");
+        map.put("two", "2");
+        map.put("three", "3");
+
+        assertEquals(3, mapsStream.transfer(map).entrySet().size());
+        assertEquals(1, mapsStream.transfer(map).get("One"));
+
     }
 }
