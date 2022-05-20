6fb2a5e9ab513bcce944ebd0d0e922fa67548542
imadcollin <imad.collin@gmail.com>
2022-05-20 14:41:55 +0200

    Add main for mutex

--- multithreading/src/main/java/lab3/PhilosopherMutex.java
+++ multithreading/src/main/java/lab3/PhilosopherMutex.java
@@ -3,7 +3,8 @@ package lab3;
 import java.util.concurrent.Semaphore;
 
 public class PhilosopherMutex {
-    private static PhilosopherMutex[] phs = new PhilosopherMutex[5];
+    private static Ph[] phs = new Ph[5];
+    private static Fork[] forks = new Fork[5];
 
     static class Fork {
         public Semaphore mutex = new Semaphore(1);
@@ -21,8 +22,9 @@ public class PhilosopherMutex {
         }
 
         boolean checkStatus() {
-            return mutex.availablePermits() > 0;
+            return mutex.availablePermits() > 1;
         }
+
     }
 
     static class Ph extends Thread {
@@ -37,12 +39,12 @@ public class PhilosopherMutex {
         public void run() {
             while (true) {
                 left.grab();
-                System.out.println("Grapping left");
+                System.out.println("Picking left");
 
                 right.grab();
-                System.out.println("Grapping right");
+                System.out.println("Picking right");
 
-                eat()
+                eat();
                 left.release();
                 right.release();
                 System.out.println("Releasing left and right");
@@ -52,11 +54,47 @@ public class PhilosopherMutex {
 
         private void eat() {
             try {
-                Thread.sleep(100);
+                Thread.sleep(2000);
+                System.out.println(".......EATING........");
+
             } catch (InterruptedException e) {
                 System.out.println(e.getMessage());
             }
         }
+
+
     }
 
+    public static void main(String[] args) {
+        Fork f = new Fork();
+        for (int i = 0; i < 5; i++) {
+            forks[i] = new Fork();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            phs[i] = new Ph(forks[i], forks[((i + 1) % phs.length)]);
+            phs[i].start();
+        }
+
+        while (true) {
+            try {
+                boolean deadLock = true;
+                for (Fork fork : forks
+                ) {
+                    if (fork.checkStatus()) deadLock = false;
+                    break;
+
+                }
+
+                if (deadLock) {
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                System.out.println("something went wrong...");
+
+            }
+        }
+    }
 }
+
+
