33f146cfcda08d923675a868e243265b54c9f55e
imadcollin <imad.collin@gmail.com>
2022-06-23 18:39:54 +0200

    Add exe2

--- codingbat/src/main/java/warmup/WarmUp.java
+++ codingbat/src/main/java/warmup/WarmUp.java
@@ -15,6 +15,30 @@ public class WarmUp {
         return (!weekday || vacation) ? true : false;
     }
 
+    public static int diff21(int n) {
+        if (n > 21) return (21 - n) * 2;
+        return 21 - n;
+    }
+
+    public static boolean nearHundred(int n) {
+        if (n <= 100) {
+            return n >= 90;
+        } else if (n > 100 && n < 200) {
+            return n >= 190 || n <= 110;
+        } else if (n >= 200 && n <= 300) {
+            return n > 290 || n <= 210;
+
+        }
+        return false;
+
+    }
+
+    public static boolean nearHundred2(int n) {
+        return ((Math.abs(100 - n) <= 10) ||
+                (Math.abs(200 - n) <= 10));
+    }
+
+
     @Test
     public void testSleepIn() {
         assertEquals(sleepIn(false, false), true);
@@ -28,4 +52,28 @@ public class WarmUp {
         assertEquals(sleepIn2(true, false), false);
         assertEquals(sleepIn2(false, true), true);
     }
+
+
+    @Test
+    public void testDiff21() {
+        assertEquals(diff21(19), 2);
+        assertEquals(diff21(10), 11);
+        assertEquals(diff21(21), 0);
+    }
+
+    @Test
+    public void testmean() {
+        assertEquals(nearHundred(93), true);
+        assertEquals(nearHundred(90), true);
+        assertEquals(nearHundred(89), false);
+    }
+
+    @Test
+    public void testmean2() {
+        assertEquals(nearHundred2(93), true);
+        assertEquals(nearHundred2(90), true);
+        assertEquals(nearHundred2(89), false);
+
+    }
+
 }
