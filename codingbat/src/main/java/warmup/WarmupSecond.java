104da6b7284e3f8d218504eccb23dda5ec565a02
imadcollin <imad.collin@gmail.com>
2022-07-01 19:32:48 +0200

    Add revers example

--- codingbat/src/main/java/warmup/WarmupSecond.java
+++ codingbat/src/main/java/warmup/WarmupSecond.java
@@ -17,10 +17,35 @@ public class WarmupSecond {
 
     public boolean posNeg2(int a, int b, boolean negative) {
         if (negative) {
-            return  (a < 0 && b < 0) ;
+            return (a < 0 && b < 0);
         }
-        return  ((a > 0 && b < 0) || (a < 0 && b > 0)) ;
+        return ((a > 0 && b < 0) || (a < 0 && b > 0));
     }
+
+    public String frontBack(String str) {
+        String newStr = "";
+        char a = str.charAt(0);
+        char b = str.charAt(str.length() - 1);
+        if (str != null && str.trim().length() > 0) {
+            str = str.replace("" + a, "" + b);
+            str = str.substring(0, str.length() - 1);
+            str = str.concat("" + a);
+        }
+        return str;
+    }
+
+    public String reverse(String str) {
+        char[] arr = new char[str.length()];
+        for (int i = 0; i < str.length() - 1; i++) {
+            arr[i] = str.charAt(i);
+        }
+        String s = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            s += str.charAt(i);
+        }
+        return s;
+    }
+
 
     @Test
     void testPosNeg() {
@@ -34,5 +59,19 @@ public class WarmupSecond {
         assertEquals(posNeg2(1, -1, false), true);
         assertEquals(posNeg2(-1, 1, false), true);
         assertEquals(posNeg2(-4, -5, true), true);
+    }
+
+    @Test
+    void testStringReplace2() {
+        assertEquals("cba", frontBack("abc"));
+        assertEquals("aavJ", frontBack("Java"));
+    }
+
+    @Test
+    void testReverse() {
+        assertEquals("cba", reverse("abc"));
+        assertEquals("asdf", reverse("fdsa"));
+        assertEquals("asdf", reverse("fdsa"));
+        assertEquals("welcome", reverse("emoclew"));
     }
 }
